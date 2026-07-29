15a1387949c2a218ee73ae5fb4291fd23b24a12f
Rishi Raj <rishi.msrit@gmail.com>
2026-07-29 18:55:17 +0530

    [20] [Time Beats: 100.00%] [Memory Beats: 38.40%] - LeetPush

---
+++ 20-Valid-Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('|| s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(!st.empty() && s[i]==')' && st.top()== '('){st.pop();}
+                else if(!st.empty() && s[i]=='}' && st.top()== '{'){st.pop();}
+                else if(!st.empty() && s[i]==']' && st.top()== '['){st.pop();}
+                else{st.push(s[i]);}
+            }
+        }
+        return st.empty();
+        
+    }
+};
