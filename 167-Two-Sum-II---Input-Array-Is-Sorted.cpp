ba440df3718c6dfd852905e59f6709a44f5d3acf
Rishi Raj <rishi.msrit@gmail.com>
2026-07-29 14:41:03 +0530

    [167] [Time Beats: 100.00%] [Memory Beats: 34.39%] - LeetPush

--- 167-Two-Sum-II---Input-Array-Is-Sorted.cpp
+++ 167-Two-Sum-II---Input-Array-Is-Sorted.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        unordered_map<int, int> hash;
+        int left = 0;
+        int right = numbers.size() - 1;
 
-        for (int i = 0; i < numbers.size(); i++) {
-            int complement = target - numbers[i];
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
 
-            if (hash.find(complement) != hash.end()) {
-                return {hash[complement]+1, i+1};
-            }
+            if (sum == target)
+                return {left + 1, right + 1};
 
-            hash[numbers[i]] = i;
+            else if (sum < target)
+                left++;
+
+            else
+                right--;
         }
 
         return {};
