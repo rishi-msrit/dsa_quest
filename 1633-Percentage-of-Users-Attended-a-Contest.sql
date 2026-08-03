1850d810cac9ed47681e75575afefecdf11aa7da
Rishi Raj <rishi.msrit@gmail.com>
2026-08-03 19:22:38 +0530

    [1633] [Time Beats: 76.32%] - LeetPush

---
+++ 1633-Percentage-of-Users-Attended-a-Contest.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT r.contest_id, ROUND((COUNT(r.user_id)/(SELECT COUNT(*)FROM Users))*100,2)  as percentage FROM Register r 
+LEFT JOIN Users u ON r.user_id = u.user_id
+
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id 
