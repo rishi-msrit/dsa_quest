ac233f2136dc7d2b3d358413ec6ffe358a21e42a
Rishi Raj <rishi.msrit@gmail.com>
2026-09-19 15:55:33 +0530

    [189] [Time Beats: 100.00%] [Memory Beats: 38.55%] - LeetPush

--- 189-Rotate-Array.cpp
+++ 189-Rotate-Array.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     void helper(vector<int>& nums, int n, int m){
-        int left = n;
-        int right = m;
-        while (left < right){
-            swap(nums[left], nums[right]);
-            left++;
-            right--;
-        }
-        
+        while (n < m){
+            swap(nums[n++], nums[m--]);
+        }  
     }
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
+        k %= nums.size();
         helper(nums,0,nums.size()-1);
         helper(nums,0,k-1);
         helper(nums,k,nums.size()-1);
