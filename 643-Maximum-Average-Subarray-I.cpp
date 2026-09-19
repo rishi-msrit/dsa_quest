dcea2589d4048a6cccd61ccefd527f170070bde2
Rishi Raj <rishi.msrit@gmail.com>
2026-09-19 11:10:39 +0530

    [643] [Time Beats: 100.00%] [Memory Beats: 86.61%] - LeetPush

--- 643-Maximum-Average-Subarray-I.cpp
+++ 643-Maximum-Average-Subarray-I.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        int left=0;
-        int right=k;
-        double sum=0;
-        double avg = 0;
-        double newavg = 0;
-        double newsum = 0;
-        for(int i=0;i<k;i++){
-            sum += nums[i];
-        }
-        avg = sum/k;
-        newavg = avg;
-        newsum =sum;
+        int sum = 0;
 
-        for(int j=k; j < nums.size(); j++){
-            newsum = newsum - nums[j-k] + nums[j];
-            
-            newavg = newsum /k;
-            avg = max(newavg, avg);
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+
+        double ans = sum;
+
+        for (int i = k; i < nums.size(); i++) {
+            sum += nums[i] - nums[i - k];
+            ans = max(ans, double(sum));
         }
-        return avg; 
+
+        return ans / k;
     }
 };
