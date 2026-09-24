4c57055c16108f6c3510543c725fd500dc9504c6
Rishi Raj <rishi.msrit@gmail.com>
2026-09-24 22:50:57 +0530

    [26] [Time Beats: 100.00%] [Memory Beats: 97.79%] - LeetPush

--- 26-Remove-Duplicates-from-Sorted-Array.cpp
+++ 26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int k = 1;
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[i - 1])
-                nums[k++] = nums[i];
+        if(nums.size() == 0) return 0;
+        int res = 1;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[i - 1]){
+                nums[res] = nums[i];
+                res++;
+            }
         }
-        return k;
+        return res;
     }
 };
