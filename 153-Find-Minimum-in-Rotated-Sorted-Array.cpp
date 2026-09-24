b88c382a5e945349bf65bdd895cdee818a45934d
Rishi Raj <rishi.msrit@gmail.com>
2026-09-24 16:47:27 +0530

    [153] [Time Beats: 100.00%] [Memory Beats: 79.67%] - LeetPush

--- 153-Find-Minimum-in-Rotated-Sorted-Array.cpp
+++ 153-Find-Minimum-in-Rotated-Sorted-Array.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         int left = 0, right = nums.size() - 1;
-
-        while(left < right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if(nums[mid] < nums[right]) {
+            if (nums[mid] < nums[right]) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-
         return nums[left];
     }
 };
