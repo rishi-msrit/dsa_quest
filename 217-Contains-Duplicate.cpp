20b768c4ee6106513977477b218e2b29d90e4585
Rishi Raj <rishi.msrit@gmail.com>
2026-09-24 15:59:45 +0530

    [217] [Time Beats: 83.72%] [Memory Beats: 99.63%] - LeetPush

--- 217-Contains-Duplicate.cpp
+++ 217-Contains-Duplicate.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-    unordered_set<int> st;
-    for(int num : nums)
-    {
-    if(st.find(num) != st.end())   
-        return true;
+bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int> st;
+//         for (int num : nums) {
+//             if (st.find(num) != st.end())
+//                 return true;
 
-    st.insert(num);                
-    }
-
-    return false;
-      
+//             st.insert(num);
+//         }
+//         return false;
+//     }
+// };
+        if(nums.size() <= 1) return false;
+        
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
     }
 };
