7f10f0f08378c57c1ec89938c05837de852342af
Rishi Raj <rishi.msrit@gmail.com>
2026-09-24 16:23:57 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 96.77%] - LeetPush

--- 33-Search-in-Rotated-Sorted-Array.cpp
+++ 33-Search-in-Rotated-Sorted-Array.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int left, int right, int target) {
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    int search(vector<int>& nums, int target) {
+
+        int start = 0, end = nums.size() - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
             if (nums[mid] == target)
                 return mid;
-            else if (nums[mid] < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
+
+            // Check if left part is sorted
+            if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1; // move left
+                } else {
+                    start = mid + 1; // move right
+                }
+            }
+            // Right part is sorted
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1; // move right
+                } else {
+                    end = mid - 1; // move left
+                }
+            }
         }
 
         return -1;
-    }
-
-    int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] > nums[right])
-                left = mid + 1;
-            else
-                right = mid;
-        }
-
-        int pivot = left;
-
-        int result = helper(nums, pivot, nums.size() - 1, target);
-
-        if (result != -1)
-            return result;
-
-        return helper(nums, 0, pivot - 1, target);
     }
 };
