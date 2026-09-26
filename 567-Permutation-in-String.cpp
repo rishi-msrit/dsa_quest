9875e0d9686bf8fe36393d70c5518439d175ab04
Rishi Raj <rishi.msrit@gmail.com>
2026-09-26 17:07:43 +0530

    [567] [Time Beats: 73.80%] [Memory Beats: 46.08%] - LeetPush

--- 567-Permutation-in-String.cpp
+++ 567-Permutation-in-String.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.size(), n2 = s2.size();
-        if (n2 < n1) return false;
+      
+        if (s1.size() > s2.size())
+            return false;
 
-        vector<int> c1(26, 0), c2(26, 0);
+        vector<int> count1(26, 0);
+        vector<int> count2(26, 0);
 
-        for (int i = 0; i < n1; i++) {
-            c1[s1[i] - 'a']++;
-            c2[s2[i] - 'a']++;
+        for (int i = 0; i < s1.size(); i++) {
+            count1[s1[i] - 'a']++;
+            count2[s2[i] - 'a']++;
         }
 
-        if (c1 == c2) return true;
+        if (count1 == count2)
+            return true;
 
-        for (int i = n1; i < n2; i++) {
-            c2[s2[i] - 'a']++;
-            c2[s2[i - n1] - 'a']--;
+        // Sliding window
+        for (int i = s1.size(); i < s2.size(); i++) {
 
-            if (c1 == c2) return true;
+            count2[s2[i] - 'a']++;
+            count2[s2[i - s1.size()] - 'a']--;
+
+            if (count1 == count2)
+                return true;
         }
 
         return false;
     }
 };
+   
