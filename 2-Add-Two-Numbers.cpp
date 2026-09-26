f2054faa54ca947c24886cb308445b46127e3307
Rishi Raj <rishi.msrit@gmail.com>
2026-09-26 20:19:31 +0530

    [2] [Time Beats: 100.00%] [Memory Beats: 75.75%] - LeetPush

--- 2-Add-Two-Numbers.cpp
+++ 2-Add-Two-Numbers.cpp
@@ -1,51 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
- class Solution {
+class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* head = nullptr;
-        ListNode* temp = nullptr;
-
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr) {
-            int val1 = (l1 != nullptr) ? l1->val : 0;
-            int val2 = (l2 != nullptr) ? l2->val : 0;
-
-            int sum = val1 + val2 + carry;
-
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-
-            if (head == nullptr) {
-                head = newNode;
-                temp = head;
-            } else {
-                temp->next = newNode;
-                temp = temp->next;
+        ListNode* dummy = new ListNode(); 
+        ListNode* cur = dummy;     
+        int carry = 0; 
+        while (l1 || l2 || carry) {
+            int sum = carry;     
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;      
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;        
             }
 
-            if (l1 != nullptr)
-                l1 = l1->next;
-
-            if (l2 != nullptr)
-                l2 = l2->next;
+            carry = sum / 10;               
+            cur->next = new ListNode(sum % 10); 
+            cur = cur->next;       
         }
 
-        if (carry != 0) {
-            temp->next = new ListNode(carry);
-        }
-
-        return head;
+        return dummy->next;
     }
 };
