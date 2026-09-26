3018aa75042bdebb3d946c53a56a0312592bc9bc
Rishi Raj <rishi.msrit@gmail.com>
2026-09-26 13:42:15 +0530

    [3] [Time Beats: 51.19%] [Memory Beats: 58.48%] - LeetPush

--- 3-Longest-Substring-Without-Repeating-Characters.cpp
+++ 3-Longest-Substring-Without-Repeating-Characters.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //unordered_map<char, int> mp; as unordered map is slow
-        int mp[128] = {0};
-        int left=0, len = 0, maxlen = 0;
-        for(int right = 0;right < s.size(); right++){
-            
-            ++mp[s[right]];
-            
-            while(mp[s[right]] > 1){
-                --mp[s[left]];
-                ++left;
+        int left = 0;
+        int res = 0;
+        unordered_map<char, int> mp;
+
+        for (int right = 0; right < s.size(); right++) {
+            mp[s[right]]++;
+
+            while (mp[s[right]] > 1) {
+                mp[s[left]]--;
+                left++;
             }
-            len = right - left + 1;
-            maxlen = max(maxlen,len);
-            
+            res = max(res, right - left + 1);
         }
-        return maxlen;
-        
+        return res;
     }
 };
