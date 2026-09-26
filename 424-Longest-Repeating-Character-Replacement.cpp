8a5598a2328927f1047472f5e93150e3e1493a20
Rishi Raj <rishi.msrit@gmail.com>
2026-09-26 13:42:03 +0530

    [424] [Time Beats: 36.17%] [Memory Beats: 40.94%] - LeetPush

--- 424-Longest-Repeating-Character-Replacement.cpp
+++ 424-Longest-Repeating-Character-Replacement.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int freq[26] = {0};
-        int left= 0, right, maxFreq =0;
+        int left = 0;
+        int res = 0;
+        int maxFreq = 0;
 
-        for(right = 0; right < s.size(); right++){
-            freq[s[right] - 'A']++;
-            maxFreq = max(maxFreq, freq[s[right]-'A']);
-            
-            if ((right - left + 1) - maxFreq > k) {
-                freq[s[left] - 'A']--;
+        unordered_map<char, int> mp;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            mp[s[right]]++;
+
+            maxFreq = max(maxFreq, mp[s[right]]);
+
+            while ((right - left + 1) - maxFreq > k) {
+                mp[s[left]]--;
                 left++;
+            }
+            res = max(res, right - left + 1);
         }
-        }
-        //return right - left + 1;
-        return s.size() - left;        
+
+        return res;
     }
 };
